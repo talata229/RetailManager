94942078567901402784397af0d4693d25451171
tranquang229 <tranquang229@gmail.com>
2020-03-22 16:19:32 +0700

    Register Db table

---
+++ TRMData/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] NVARCHAR(128) PRIMARY KEY NOT NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [EmailAdress] NVARCHAR(256) NOT NULL, 
+    [CretedDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
